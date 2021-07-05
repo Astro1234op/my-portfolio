9bbf3229d0dd1a365c6266dfbeab3cb6097673a6
Astro1234op <85688225+Astro1234op@users.noreply.github.com>
2021-07-05 13:02:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function spanelem() {
+    window.alert("This is an SPAN element");
+  }
+  
+  function one() {
+    window.alert("Earth is the third planet from the Sun and the only astronomical object known to harbor and support life. About 29.2% of Earth's surface is land consisting of continents and islands. The remaining 70.8% is covered with water, mostly by oceans, seas, gulfs, and other salt-water bodies, but also by lakes, rivers, and other freshwater, which together constitute the hydrosphere. Much of Earth's polar regions are covered in ice. Earth's outer layer is divided into several rigid tectonic plates that migrate across the surface over many millions of years, while its interior remains active with a solid iron inner core, a liquid outer core that generates Earth's magnetic field, and a convective mantle that drives plate tectonics.");
+    
+  }
+  function two() {
+    window.alert("Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass more than two and a half times that of all the other planets in the Solar System combined, but slightly less than one-thousandth the mass of the Sun. Jupiter is the third-brightest natural object in the Earth's night sky after the Moon and Venus. It has been observed since pre-historic times and is named after the Roman god Jupiter, the king of the gods, because of its observed size.");
+    
+  }
+  function three() {
+    window.alert("Uranus is the seventh planet from the Sun. Its name is a reference to the Greek god of the sky, Uranus, who, according to Greek mythology, was the great-grandfather of Ares (Mars), grandfather of Zeus (Jupiter) and father of Cronus (Saturn). It has the third-largest planetary radius and fourth-largest planetary mass in the Solar System.");
+    
+  }
+  function four() {
+    window.alert("TON 618 is a hyperluminous, broad-absorption-line, radio-loud quasar located near the border of the constellations Canes Venatici and Coma Berenices, approximately 10.4 billion light-years from Earth. It possesses one of the most massive black holes ever found, with a mass of 66 billion M☉.[2]")
+  }
+  function five() {
+    window.alert("Sagittarius A*  is a bright and very compact astronomical radio source at the Galactic Center of the Milky Way. It is located near the border of the constellations Sagittarius and Scorpius, about 5.6° south of the ecliptic.[3] Sagittarius A* is the location of a supermassive black hole,[4][5][6] similar to those at the centers of most, if not all, spiral and elliptical galaxies.");
+    
+  }
+  function six() {
+    window.alert("M87 is an enormous elliptical galaxy located 55 million light-years from us. Its central supermassive black hole is one of the most massive ones known, and, like a cosmic searchlight, a jet of subatomic particles traveling nearly at the speed of light is streaming out of it, as far out as about 6,000 light-years.");
+    
+  }
